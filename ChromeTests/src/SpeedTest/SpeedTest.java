74fc20c04e38445502c796266a95c865cf60fe2e
mukes <mukes@AsWin>
2025-05-19 00:16:40 +0530

    Chrmomedriver and basic test class

---
+++ ChromeTests/src/SpeedTest/SpeedTest.java
@@ -0,0 +1,27 @@
+package SpeedTest;
+
+import org.openqa.selenium.chrome.*;
+import org.openqa.selenium.WebDriver;
+
+public class SpeedTest {
+    public static void main(String[] args) {
+        // Set the path to chromedriver executable
+        System.setProperty("webdriver.chrome.driver", "chromedriver/chrome_test/chromedriver.exe");
+
+        // Initialize ChromeDriver
+        WebDriver driver = new ChromeDriver();
+
+        // Open fast.com
+        driver.get("https://fast.com");
+
+        // Optional: wait for a few seconds to see the speed test start
+        try {
+            Thread.sleep(10000); // 10 seconds
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // Close the browser
+        driver.quit();
+    }
+}
